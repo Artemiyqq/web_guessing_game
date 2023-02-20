b5b1cbcc54305e5429b381dbe2bcf2da9abdad40
Artemiyqq <artom.florko@gmail.com>
2023-02-20 16:43:48 +0200

    added togles to the password inputs. Also, a ban on entering some characters to inputs has been added.

--- static/account/script.js
+++ static/account/script.js
@@ -1,5 +1,5 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
+const signUpButton = document.getElementById('sign-up');
+const signInButton = document.getElementById('sign-in');
 const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
@@ -9,3 +9,38 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+const inputField = document.getElementById("sign-up-username");
+const passwordField = document.getElementById("sign-up-password");
+const repeatPasswordField = document.getElementById("sign-up-repeat-password");
+
+
+inputField.addEventListener("input", checkInput);
+passwordField.addEventListener("input", checkInput);
+repeatPasswordField.addEventListener("input", checkInput);
+
+function checkInput(){
+	const prohibitedChars = /[!@#$%^&*() +\-=\[\]{};':"\\|,.<>\/?]/g;
+	const inputValue = this.value;
+
+	if (prohibitedChars.test(inputValue)) {
+	this.value = inputValue.replace(prohibitedChars, '');
+	}
+};
+
+const passwordToggleImg = document.getElementById('sign-up-password-toggle')
+const repeatPasswordToggle = document.getElementById('sign-up-repeat-password-toggle')
+const signInToggle = document.getElementById('sing-in-toggle')
+
+passwordToggleImg.addEventListener('click', function(){
+	togglePasswordVisibility(document.getElementById('sign-up-password'))})
+repeatPasswordToggle.addEventListener('click', function(){
+	togglePasswordVisibility(document.getElementById('sign-up-repeat-password'))})
+signInToggle.addEventListener('click', function(){
+		togglePasswordVisibility(document.getElementById('sing-in-password'))})	
+
+function togglePasswordVisibility(inputElement) {
+	const currentType = inputElement.getAttribute('type');
+  	const newType = currentType === 'password' ? 'text' : 'password';
+  	inputElement.setAttribute('type', newType);
+}
